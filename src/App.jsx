4ceda00cdf84065c6ea0d3e9f4bd6fc41a63c164
Rishi Mondal <146999057+MAVRICK-1@users.noreply.github.com>
2024-01-28 23:31:52 +0530

    added Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import LoginSignUp from "./components/LoginSignup/LoginSignUp";
 import "./App.css"
+import Navbar from "./components/Navbar/Navbar";
 
 export default function App() {
   return (
-  
+    <>
+    <Navbar/>
     <LoginSignUp/>
+    </>
   )
 }
